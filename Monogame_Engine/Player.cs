4688569dc7fbae1f710a35569adc33c209011cee
Ilirr <ilirruka98@gmail.com>
2022-10-02 16:21:24 +0200

    up

--- Monogame_Engine/Player.cs
+++ Monogame_Engine/Player.cs
@@ -13,7 +13,7 @@ namespace Monogame_Engine
         public static Player Instance { get { return lazy.Value; } }
 
         public Input input;
-        private float speed = 2;
+        private float speed = 100;
         public Player()
         {
         }
@@ -27,26 +27,40 @@ namespace Monogame_Engine
             m_Sprite = this.GetComponent<Sprite>();
             m_RB = this.GetComponent<Rigidbody2D>();
             m_Sprite.m_Texture2D = Game1.Instance.Content.Load<Texture2D>("Character");
+            Console.Write("a");
 
             input = new Input();
 
         }
         public override void Update(GameTime gameTime)
         {
+            float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
             input.lastState = input.currentState;
             input.currentState = Keyboard.GetState();
-            if (input.IsKeyUp(Keys.W))
+            if (input.IsHeld(Keys.W))
             {
-                position.Y += 30;
+                position.Y -= speed * delta;
             }
+            if (input.IsHeld(Keys.D))
+            {
+                position.X += speed * delta;
+            }
+            if (input.IsHeld(Keys.A))
+            {
+                position.X -= speed * delta;
+            }
+             if (input.IsHeld(Keys.S))
+            {
+                position.Y += speed * delta;
+            }
+            System.Console.WriteLine("HA");
+
 
             m_RB.velocity = Vector2.Zero;
         }
         
         public override void Awake()
         {
-
-
             Console.WriteLine("Player is Awake");
         }
         public override void Destroy()
